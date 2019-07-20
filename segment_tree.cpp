ed1ac3612cb547450ad2f00d2df3b953034cae5a
Pretam Ray <45117649+raypretam@users.noreply.github.com>
2019-07-20 15:41:28 +0530

    added update function

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -12,6 +12,22 @@ void buildTree(int *arr,int* tree,int start,int end,int treeNode){
   tree[treeNode]=tree[2*treeNode]+tree[2*treeNode+1];
 }
 
+void update(int* arr,int* temp,int treeNode,int start,int end,int idx,int value){
+  if(start==end){
+    arr[idx]=value;
+    tree[idx]=value;
+    return;
+  }
+  int mid=(start+end)/2;
+  if(idx<mid){
+    update(arr,tree,2*treeNode,start,mid,idx,value);
+  }
+  else{
+    update(arr,tree,2*treeNode+1,mid+1,end,idx,value);
+  }
+  tree[treeNode]=tree[2*treeNode]+tree[2*treeNode+1];
+}
+
 int main(){
   int n;
   cout<<"Enter the number of elements: ";
@@ -23,6 +39,7 @@ int main(){
   }
   int* tree=new int[2*n];
   buildTree(arr,tree,0,n-1,1);
+  update(arr,tree,0,n-1,4,10);
   cout<<"The elements are\n";
   for(int i=1;i<2*n;i++){
     cout<<tree[i]<<endl;
