11c8d96bc49a49042858b07a12163dc4d2d3b5c4
Pretam Ray <45117649+raypretam@users.noreply.github.com>
2019-07-22 09:04:29 +0530

    Add files via upload

--- linked_list.c
+++ linked_list.c
@@ -5,12 +5,48 @@ struct Node{
   struct Node* next;
 };
 struct Node* head=NULL;
+//Insert in reverse order
 void Insert(int x){
   struct Node* temp=(Node*)malloc(sizeof(struct Node));
   temp->data=x;
   temp->next=head;
   head=temp;
 }
+//Insert at the front of the linked List
+void insertFront(int x){
+  struct Node* temp=(Node*)malloc(sizeof(struct Node));
+  temp->data=x;
+  temp->next=head;
+  head=temp;
+}
+
+//Insert after a given Node
+void insertNodeAfter(strcut Node* prev_Node,int x){
+  struct Node* temp=(Node*)malloc(sizeof(struct Node));
+  if(prev_Node==NULL){
+    printf("Cannot be inserted");
+    return;
+  }
+  temp->next=prev_Node->next;
+  prev_Node=temp;
+}
+//Insert at the end of the linked List
+void insertEnd(struct Node** head,int x){
+  struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    struct Node *last = *head_ref;
+    new_node->data  = new_data;
+    new_node->next = NULL;
+    if (*head_ref == NULL)
+    {
+       *head_ref = new_node;
+       return;
+    }
+    while (last->next != NULL)
+        last = last->next;
+    last->next = new_node;
+    return;
+}
+//Printing the linked List 
 void Print(){
   struct Node* temp=head;
   printf("List is: ");
